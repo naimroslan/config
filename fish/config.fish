fe054ec921805b704d3318459cc03bf787d92d47
Adrian Low <adrianlow1998@gmail.com>
2025-09-05 19:41:05 +0800

    fixed hyperland crashing on boot

--- fish/config.fish
+++ fish/config.fish
@@ -18,7 +18,16 @@ function fish_greeting
     # fastfetch
 end
 
-# Only run tmux in a pure TTY (no X11/Wayland session)
-if test -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY"
+function run_tmux
+    command -qs tmux; or return
+    if set -q TMUX
+        tmux switch-client -t 1
+        return
+    end
+    exec tmux new -A -s 1 -f ~/.config/tmux/tmux.conf
+end
+
+# Only on pure TTY login shells
+if status is-interactive; and status is-login; and not set -q DISPLAY; and not set -q WAYLAND_DISPLAY; and string match -q -r '^/dev/tty[1-6]$' (tty)
     run_tmux
 end
