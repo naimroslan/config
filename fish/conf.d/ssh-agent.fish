20900f6cd11e608f2b0e1c75eaa1d9386ba8471b
Adrian Low <adrianlow1998@gmail.com>
2025-09-24 19:31:52 +0800

    updated config

--- fish/conf.d/ssh-agent.fish
+++
@@ -1,7 +0,0 @@
-# SSH agent setup for Fish shell
-# Simple approach: just try to add the key to any available agent
-
-# Add SSH key if it exists (this will work with any running SSH agent)
-if test -f ~/.ssh/adrian_rooftop_ed25519
-    SSH_AUTH_SOCK=/run/user/1000/ssh-agent.socket ssh-add ~/.ssh/adrian_rooftop_ed25519 2>/dev/null
-end
