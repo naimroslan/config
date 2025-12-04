d37ef19ec0fbeb538be1d7292213a589b85807ea
Adrian Low <adrianlow1998@gmail.com>
2025-12-04 17:02:38 +0800

    Update ghp function to check upstream before setting

--- fish/conf.d/ghp.fish
+++ fish/conf.d/ghp.fish
@@ -1,5 +1,11 @@
 function ghp
     set -l message (string join " " $argv)
     set -l current_branch (git branch --show-current)
-    git add . && git commit -m "$message" && git push --set-upstream origin $current_branch
+
+    # Check if upstream is already set
+    if git rev-parse --abbrev-ref --symbolic-full-name @{u} &>/dev/null
+        git add . && git commit -m "$message" && git push
+    else
+        git add . && git commit -m "$message" && git push --set-upstream origin $current_branch
+    end
 end
