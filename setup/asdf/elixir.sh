89d34445ab91c665154c6f136651701a91f77242
Adrian Low <adrian@rooftop.my>
2026-01-26 17:56:39 +0800

    updated config

---
+++ setup/asdf/elixir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+asdf plugin add elixir
+asdf install elixir latest
--- setup/asdf/erlang.sh
+++ setup/asdf/erlang.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-# erlang is needs to be installed to compile elixir
 asdf plugin add erlang
 asdf install erlang latest
-
-asdf plugin add elixir
-asdf install elixir latest
