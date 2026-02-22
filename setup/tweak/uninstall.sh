c91e2d450270f6c19262f310b504899937321328
Adrian Low <adrianlow1998@gmail.com>
2026-02-22 23:31:58 +0800

    Delete uninstall.sh

--- setup/tweak/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-pacman -Q alacritty &>/dev/null && sudo pacman -Rns --noconfirm alacritty || true
-pacman -Q nano-syntax-highlighting &>/dev/null && sudo pacman -Rns --noconfirm nano-syntax-highlighting || true
-pacman -Q nano &>/dev/null && sudo pacman -Rns --noconfirm nano || true
